c56cfa3d2bf57bccef576d755b32caafffcc3740
Sanchoserik <Sanchoserik@gmail.com>
2018-10-09 11:30:15 +0300

    v0.2.0a
    Added mainmenu
    removed System.Threading.Tasks for Unity

--- Assets/Code/Items/Potions/earthUpMix4U.cs
+++ Assets/Code/Items/Potions/earthUpMix4U.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+ 
 using Adventure.Effects;
 
 namespace Adventure.Items.Potions
--- Assets/Code/Skills/ActionScripts/a_OverchargeScript.cs
+++ Assets/Code/Skills/ActionScripts/a_OverchargeScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+ 
 using Adventure.ScriptHelper;
 
 namespace Adventure.Skills.ActionScripts
