6a5dcfc0acbe7ed64824742a3ec711ef8fc7c203
Sanchoserik <Sanchoserik@gmail.com>
2018-12-08 19:33:38 +0200

    v0.2.3b1
    finnished skills loader
    localisation file skillLocUA.xml bugfixes

--- Assets/Resources/xml/Loader.cs
+++ Assets/Resources/xml/Loader.cs
@@ -6,9 +6,9 @@ using System.Xml.Serialization; //Needed for XML Functionality
 using System.IO;
 using System.Xml.Linq; //Needed for XDocument
 
+// no namespace
 public class Loader : MonoBehaviour
 {
-    XDocument xmlDoc; //create Xdocument. Will be used later to read XML file 
     List <skillData> data = new List <skillData>();   
     //Initialize List of XMLData objects.
     bool finishedLoading = false;
@@ -19,8 +19,8 @@ public class Loader : MonoBehaviour
                                        //Loads XML File. Code below.
                                        // StartCoroutine("AssignData");         
                                        //Starts assigning XML data to data List. Code below
-        LoadXML();
-        int xx = 20;
+        LoadXML(); 
+           int xx = 20;
     }
     void Update()
     {
@@ -32,18 +32,22 @@ public class Loader : MonoBehaviour
         //}
     }
 
-    void LoadXML()
+    void LoadXML() // param localisation!
     {
-        IEnumerable<XElement> items; // <skill> tag
-        //Create an Ienumerable list. Will be used to store XML Items.
-        IEnumerable<XElement> skilllevels; // <l> tag
-        IEnumerable<XElement> leveldata; // <v tag>
-        //Assigning Xdocument xmlDoc. Loads the xml file from the file path listed. 
-        xmlDoc = XDocument.Load("Assets/Resources/xml/skills.xml");
-        //This basically breaks down the XML Document into XML Elements. Used later.
+        IEnumerable<XElement> items; // <skill> tag        
+        IEnumerable<XElement> skillLevels; // <l> tag
+        IEnumerable<XElement> skillLoc; // localisation <skill> tag
 
-        items = xmlDoc.Descendants("skills").Elements();
-        skilllevels = xmlDoc.Descendants("skill").Elements();
+        //Assigning Xdocument xmlDoc. Loads the xml file from the file path listed. 
+        XDocument skillsXDoc = XDocument.Load("Assets/Resources/xml/skills.xml");
+       
+              // UA LOCALISATION by deffault
+        XDocument skillsLocXDoc = XDocument.Load("Assets/Resources/xml/skillsLocUA.xml");
+        
+        items = skillsXDoc.Descendants("skills").Elements();
+        skillLevels = skillsXDoc.Descendants("skill").Elements();
+
+        skillLoc = skillsLocXDoc.Descendants("skills").Elements(); // get localisation 
         //leveldata = xmlDoc.Descendants("l").Elements();
         //find skill data
         foreach (XElement item in items)
@@ -55,50 +59,71 @@ public class Loader : MonoBehaviour
 
             Dictionary<int, string> lDataEne = new Dictionary<int, string>();
             Dictionary<int, string> lDataAP = new Dictionary<int, string>();
-            Dictionary<string, string> vValues = new Dictionary<string, string>();
-
-            bool moveforward = true;
-            bool firstcheck = false;
+            // int is skill level
+            List<Dictionary<string, string>> vValues = new List<Dictionary<string, string>>();         
+       
             int levelIter = 1;
             foreach (XElement el in item.Elements("l"))
-            {
-               firstcheck = true;
-              
+            {        
+               // level AP&Ene cost values
                lDataEne.Add(levelIter, el.Attribute("pEne").Value);
                lDataAP.Add(levelIter, el.Attribute("pAP").Value);
- 
-               foreach (XElement vTag in el.Nodes())
-               {               
-                 vValues.Add(vTag.Attribute("name").Value.Trim(), vTag.Element("v").Value.Trim());
-               }              
-               ++levelIter;
-            }
-            
-            int x = 0;
-            //item.
+                // special level values in "v" element
+                if (el.Nodes() != null)
+                { vValues.Add(new Dictionary<string, string>());
+                    foreach (XElement vTag in el.Nodes())
+                    {
+                       vValues[vValues.Count-1].Add(vTag.Attribute("name").Value, vTag.Value);                     
+                    }
+                }
+               ++levelIter;              
+            }//end foreach
+            //saving data 
+            data.Add(new skillData(_levels, _name, _scname, lDataAP, lDataEne, vValues));                 
+        }
 
-            data.Add(new skillData(_levels, _name, _scname));     
-           // Debug.Log("name =" + _name + "  levels = "+ _levels + "  scname = " + _scname);          
-        }       
+        int iter = 0; // iter
+        foreach (XElement item in skillLoc)
+        {
+            data[iter].locName = item.Attribute("locname").Value;
+            data[iter].sDescr = item.Attribute("descr").Value;
+            List<string> levelsDescr = new List<string>();
+            foreach (XElement el in item.Elements("l"))
+            {
+                levelsDescr.Add(el.Value);
+            }
+            data[iter].locDescr = levelsDescr;
+            ++iter;
+        }
+
     }
-}
+}//end class loader
 // This class is used to assign our XML Data to objects in a list so we can call on them later.
 public class skillData {
 
     public short levels;
-    public string name, scname;
+    public string name;
+    public string scName;
+    public Dictionary<int, string> lcAP;
+    public Dictionary<int, string> lcEne;
+    public List<Dictionary<string, string>> vValues;
+    //localisation
+    public string locName;
+    public List<string> locDescr;
+    public string sDescr; // skil description
+
     // Create a constructor that will accept multiple arguments that can be assigned to our variables. 
-    public skillData(short _levels, string _name, string _scname)
+    public skillData(short _levels, string _name, string _scname, Dictionary<int, string> _lcAP, 
+        Dictionary<int, string> _lcEne, List<Dictionary<string, string>> _vValues) 
+     //   string _locName, List<string> _locDescr)
     {
-         levels = _levels;
-         name = _name;
-         scname = _scname;     
+        levels = _levels;
+        name = _name;
+        scName = _scname;
+        lcAP = _lcAP;
+        lcEne = _lcEne;
+        vValues = _vValues;
+     //   locName = _locName;
+     //   locDescr = _locDescr;
     }
-
-    public void skillLevels()
-    {
-
-    }
-
-
 }
