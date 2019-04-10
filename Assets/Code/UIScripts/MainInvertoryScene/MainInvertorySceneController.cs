52e4874d98836b0be8491d7f9daba3bc67d71567
Sanchoserik <Sanchoserik@gmail.com>
2019-04-10 09:17:36 +0300

    v0.2.5b
    working with MainInvertory scene

---
+++ Assets/Code/UIScripts/MainInvertoryScene/MainInvertorySceneController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Code.UIScripts.MainInvertoryScene
+{
+    public class MainInvertorySceneController : MonoBehaviour
+    {
+        private void Start()
+        {
+            
+
+        }
+
+        private void Update()
+        {
+           
+            
+        }
+    }
+}
