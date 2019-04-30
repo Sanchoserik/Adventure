f5dd0c5fd2a497439617d6dc2882f35e3f200a52
Sanchoserik <Sanchoserik@gmail.com>
2019-04-30 11:38:57 +0300

    v0.2.5d
    - working with toolTip in MainInventory scene

---
+++ Assets/Code/UIScripts/MainInvertoryScene/FollowMouse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Code.UIScripts.MainInvertoryScene
+{
+    class FollowMouse : MonoBehaviour
+    {
+        private void Update()
+        {
+            transform.position = Input.mousePosition;
+        }
+
+    }
+}
