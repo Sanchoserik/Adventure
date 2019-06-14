da174f06915456d929d7fb01bfe151641b67508e
Sanchoserik <Sanchoserik@gmail.com>
2019-06-14 10:33:40 +0300

    v0.2.6b
    - added changeSkillBranch in CharacterMenu scene
    - added SkillGeneralToolTip in CharacterMenu scene

---
+++ Assets/Code/UIScripts/CharacterMenuScene/SkillTree/SkillBranchChange.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Code.UIScripts.CharacterMenuScene.SkillTree
+{
+    public class SkillBranchChange : MonoBehaviour
+    {
+        public GameObject tacticianBranch;
+        public GameObject warriorBranch;
+        public GameObject assassinBranch;
+        public GameObject bruiserBranch;
+
+        public void changeBranch(GameObject branch)
+        {
+            tacticianBranch.SetActive(false);
+            warriorBranch.SetActive(false);
+            assassinBranch.SetActive(false);
+            bruiserBranch.SetActive(false);
+
+            branch.SetActive(true);
+        }
+
+    }
+}
