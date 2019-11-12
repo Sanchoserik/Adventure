2c6708edb3f0f50c194e38d9083d7825557f41d6
Alexander <36515326+Sanchoserik@users.noreply.github.com>
2019-11-12 09:07:57 +0200

    v0.3.1b
    - testing enemy spawning

--- Assets/Code/UIScripts/BattleArenaScene/BAGameController.cs
+++ Assets/Code/UIScripts/BattleArenaScene/BAGameController.cs
@@ -5,13 +5,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Tilemaps;
 
 namespace Assets.Code.UIScripts.BattleArenaScene
 {
-    public class BAGameController
+    public class BAGameController : MonoBehaviour
     {
+        public Tilemap battlefieldTilemap;
+
+        public GameObject enemySpritePrefab;
+        
         public C_Hero mainHero;
         public Battlefield arenaField;
+
+        public void Start()
+        {
+            Grid grid = battlefieldTilemap.layoutGrid;
+
+            TileBase tile = battlefieldTilemap.GetTile(new Vector3Int(3,3,1));
+
+            BoundsInt mbounds = battlefieldTilemap.cellBounds;
+
+            Instantiate(enemySpritePrefab, new Vector3(0, 0, 1),new Quaternion(), battlefieldTilemap.transform);
+            Instantiate(enemySpritePrefab, new Vector3(1, 0, 1), new Quaternion(), battlefieldTilemap.transform);
+            Instantiate(enemySpritePrefab, new Vector3(1, 1, 1), new Quaternion(), battlefieldTilemap.transform);
+            Instantiate(enemySpritePrefab, new Vector3(-2, 2, 1), new Quaternion(), battlefieldTilemap.transform);
+            Instantiate(enemySpritePrefab, new Vector3(3, 0, 1), new Quaternion(), battlefieldTilemap.transform);
+            Instantiate(enemySpritePrefab, new Vector3(0, 3, 1), new Quaternion(), battlefieldTilemap.transform);
+
+            int xx = 20;
+        }
 
     }
 }
