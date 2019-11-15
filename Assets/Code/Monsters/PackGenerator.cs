29e8ad2dbed45acf3548db31e85cc15afc06bf0a
Alexander <36515326+Sanchoserik@users.noreply.github.com>
2019-11-15 12:01:50 +0200

    v0.3.1b3
    - working with monster pack generator
    - monsters.xml added "mType" attribute

--- Assets/Code/Monsters/PackGenerator.cs
+++ Assets/Code/Monsters/PackGenerator.cs
@@ -10,13 +10,19 @@ namespace Assets.Code.Monsters
 {
     public class PackGenerator
     {
-        public enum MonstersList
+        private enum MonstersList
         {
-            NativeAirR1, NativeDarknessR1, NativeDeathR1, NativeEarthR1, NativeFireR1, NativeLifeR1, NativeLightningR1, NativeLightR1, NativePhysR1, NativeWaterR1
+            NativeAirR1, NativeDarknessR1, NativeDeathR1, NativeEarthR1, NativeFireR1, 
+            NativeLifeR1, NativeLightningR1, NativeLightR1, NativePhysR1, NativeWaterR1
         }
 
-        public readonly Dictionary<MonstersList, IMonsterFactory> mFactories;
-        public readonly List<MonstersData> monstersDataList = ResourcesManager.monstersData;
+        public enum PackTypes
+        { 
+           Fighters, Casters, Balanced, Boss  
+        }
+
+        private readonly Dictionary<MonstersList, IMonsterFactory> mFactories;      
+        private readonly List<MonstersData> monstersDataList = ResourcesManager.monstersData;
 
         public PackGenerator()
         {
@@ -28,10 +34,67 @@ namespace Assets.Code.Monsters
                 mFactories.Add(monst, factory);
             }
         }
-   
-        public void generatePack()
+           
+        public void generatePack(int packCost,
+            PackTypes packType,  int maxRank, 
+            int battlefieldSizeX, int battlefieldSizeY 
+            )
         {
+            switch (packType.ToString())
+            {
+                case "Fighters": {
+                        List<MonstersData> selectedMTypes = selectMTypes(maxRank, packType);
+                        generateFightersPack(packCost, battlefieldSizeX, battlefieldSizeY, selectedMTypes);
+                        break;
+                    }
+            }
         }
+
+        private void generateFightersPack(int packCost, int battlefieldSizeX, int battlefieldSizeY, List<MonstersData> selectedMTypes)
+        {
+            Random random = new Random();
+            
+            List<A_Monster> generatedMosters = new List<A_Monster>();
+
+            int minumumCost = Convert.ToInt32(selectedMTypes[0].monsterParameters["mPrice"]);
+            foreach (MonstersData data in selectedMTypes)
+            {
+                if (minumumCost > Convert.ToInt32(data.monsterParameters["mPrice"]))
+                    minumumCost = Convert.ToInt32(data.monsterParameters["mPrice"]);
+            }
+
+            while (packCost >= minumumCost)
+            {
+                int randIndex = random.Next(0, selectedMTypes.Count - 1);
+                int mPrice = Convert.ToInt32(selectedMTypes[randIndex].monsterParameters["mPrice"]);
+
+                if (packCost >= mPrice)
+                {
+                    
+                    generatedMosters.Add(mFactories[(MonstersList)Enum.Parse(typeof(MonstersList), 
+                        selectedMTypes[randIndex].monsterName)].createMonster(selectedMTypes[randIndex])); 
+                    packCost -= mPrice;
+                }
+            }
+
+        }
+
+        private List<MonstersData> selectMTypes(int maxRank, PackTypes packType)
+        {
+            List<MonstersData> mData = new List<MonstersData>();
+
+            foreach (MonstersData data in monstersDataList)
+            {
+                if (Convert.ToInt32(data.monsterParameters["mRank"]) <= maxRank &&
+                    data.monsterParameters["mType"].Equals("Fighter"))
+                {
+                    mData.Add(data);
+                }
+            }
+
+            return mData;
+        }
+
 
         private A_Monster addMonster(MonstersList monster, MonstersData mData) => mFactories[monster].createMonster(mData);
 
